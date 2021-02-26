b6f783703bd8f2beef9bfd416515caba9bbd235c
Qniqu <michal.konik98@gmail.com>
2021-02-26 17:56:54 +0100

    v2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Animal dog = new Animal();
-		dog.name = "Haiku";
+		dog.name = "pies1";
 		dog.Species = "dog";
 		dog.weight = 8.0;
 		dog.feed();
@@ -13,6 +13,13 @@ public class Main {
 		Iphone.Model = "65";
 		Iphone.screenSize = 4.5;
 		Iphone.OperationSystem = "IOS";
+
+		Human me = new Human();
+		me.firstName = "Michal";
+		me.lastName = "Konik";
+		me.pet = dog;
+
+		System.out.println("nazwa zwierzecia to " + me.pet.name);
 
 	}
 }
